bc0ea7979ad7bafff1573106ea3424efcd5a2f30
craftsman <ramy.abbas47@gmail.com>
2022-07-15 07:05:40 +0300

    leetcode: Maximum depth of a binary tree

---
+++ leetcode/treeMaxDepth.js
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = (root) => {
+  if (root === null) {
+    return 0;
+  }
+  if (root.left === null && root.right === null) {
+    return 1;
+  }
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
