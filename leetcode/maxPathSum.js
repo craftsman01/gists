e6cadf7f579197ffbc8b771839716776a0c1f1f5
craftsman <ramy.abbas47@gmail.com>
2022-07-23 06:57:54 +0300

    leetcode: Max Path Sum updated solution

--- leetcode/maxPathSum.js
+++ leetcode/maxPathSum.js
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -10,24 +11,26 @@
  * @param {TreeNode} root
  * @return {number}
  */
-const maxPathSum = (root) => {
-  const tree = new TreeHelper();
-  tree.calculateMaxPathSum(root);
-  return tree.globalMaxSum;
+const maxPathSum = function (root) {
+  return new TreeHelper().findMaxPathSum(root);
 };
 
 class TreeHelper {
-  constructor() {
+  findMaxPathSum(root) {
     this.globalMaxSum = -Infinity;
+    this.calculateMaxPathSum(root);
+    return this.globalMaxSum;
   }
   calculateMaxPathSum(root) {
-    if (!root) {
+    if (root === null) {
       return 0;
     }
+
     const left = Math.max(this.calculateMaxPathSum(root.left), 0);
     const right = Math.max(this.calculateMaxPathSum(root.right), 0);
     const currentMax = left + right + root.val;
-    this.globalMaxSum = Math.max(this.globalMaxSum, currentMax);
+
+    this.globalMaxSum = Math.max(currentMax, this.globalMaxSum);
 
     return Math.max(left, right) + root.val;
   }
